cfc79c27ec494e2542ae1cc13291ed59c0684860
developedByAn009 <95417288+An009@users.noreply.github.com>
2022-11-10 14:23:42 +0100

    Delete src directory

--- src/Features/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import tasksReducer from "./tasksSlice";
-
-const store = configureStore({
-    reducer:{
-        tasks:tasksReducer
-    }
-})
-export default store;
